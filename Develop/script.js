25f1b0d205a4782a79f6ff87878b0e31bb396db6
Jess Onacko <jessonacko@Jesss-Air.lan>
2022-07-20 20:01:46 +0100

    confirms reconfigured

--- Develop/script.js
+++ Develop/script.js
@@ -3,34 +3,34 @@
  * When red button is clicked, user is prompted to specify whether they would like upper case, lower case & special characters in their password
  * User is prompted to specify a length for their password between 8-128 char
  * Loop until user chooses a character length between 8-128 char
- * Password is generated based on the above
+ * Password is generated based on the above outcomes
  */
 
 // Assignment code here
 
-const resultEL = document.getElementById('result');
-const lengthEL = document.getElementById('length');
-const uppercaseEL = document.getElementById('uppercase');
-const lowercaseEL = document.getElementById('lowercase');
-const numberEL = document.getElementById('number');
-const symbolEL = document.getElementById('symbol');
-const generateEL = document.getElementById('generate');
-const clipboardEL = document.getElementById('clipboard');
+// const resultEL = document.getElementById('result');
+// const lengthEL = document.getElementById('length');
+// const uppercaseEL = document.getElementById('uppercase');
+// const lowercaseEL = document.getElementById('lowercase');
+// const numberEL = document.getElementById('number');
+// const symbolEL = document.getElementById('symbol');
+// const generateEL = document.getElementById('generate');
+// const clipboardEL = document.getElementById('clipboard');
 
-const randomFunc = {
-lower: getLowercase,
-upper: getUppercase,
-number: getNumber,
-symbol: getSpecialChar
+// const randomFunc = {
+// lower: getLowercase,
+// upper: getUppercase,
+// number: getNumber,
+// symbol: getSpecialChar
 
-};
+// };
 
-generateEL.addEventListener('click', () => {
-const length = lengthEl.vale;
+// generateEL.addEventListener('click', () => {
+// const length = lengthEl.vale;
 
-console.log(length);
+// console.log(length);
 
-})
+// })
 
 //Generator functions - these worked in the console
 
@@ -55,13 +55,6 @@ function getSpecialChar ()  {
 
 }
 
-//
-
-console.log(getSpecialChar());
-
-//  When the user first visits the page
-
-alert("Click the red button to generate a password.");
 
 // This makes the button reactive
 
@@ -69,27 +62,69 @@ var generatePassword = function () {
 
 //Prompt for length
 
-function promptForLength() {
-  let userResponse = prompt("How long would you like your password to be? (No more than 128 characters)");
-  let numberResponse = Number(userResponse);
-  while (isNaN(numberResponse) || numberResponse > 128 || numberResponse < 8) {
-      if (isNaN(numberResponse)) {
-        userResponse = prompt("That's not a number. Please enter a number.");
-      }
-      else if(numberResponse > 128) {
-        userResponse = prompt("That's too many characters.");
-      }
-      else if (numberResponse < 8) {
-        userResponse = prompt("That's not enough characters.");
-      }
 
-    }
-    numberResponse = Number(userResponse);
+let length = prompt("How long would you like your password to be?  Please choose a value of at least 8 characters and less than 128 characters.");
+let numberResponse = Number(length);
+console.log (numberResponse)
+if (length < 8 || length > 128  || isNaN(numberResponse))  {
+  alert("Please choose a value that is at least 8 characters and no more than 128 characters.");
+  return;
+} else { 
+   alert("nice one");
+}
+
+  //this asks the user to set the parameters of their password
+
+  var lowerCaseChoice = confirm("Would you like your password to contain lower case letters?  Press 'ok' if so, 'cancel' if not.");
+
+  var upperCaseChoice =confirm("Would you like your password to contain upper case letters?  Press 'ok' if so, 'cancel' if not.");
+
+  var specialCharChoice =confirm("Would you like your password to contain special characters?  Press 'ok' if so, 'cancel' if not.");
+
+  var numberChoice =confirm("Would you like your password to contain numbers?  Press 'ok' if so, 'cancel' if not.");
+
+  console.log(lowerCaseChoice, upperCaseChoice, specialCharChoice, numberChoice);
+  if (lowerCaseChoice === true) {
+
+    console.log(getLowercase())
   }
 
-{
-document.getElementById("generate").value=promptForLength;
-}
+  if (upperCaseChoice === true) {
+
+    console.log(getUppercase())
+  }
+
+  if (specialCharChoice === true) {
+
+    console.log(getSpecialChar())
+  }
+
+  if (numberChoice === true) {
+
+    console.log(getNumber())
+  }
+
+// function promptForLength() {
+//   let userResponse = prompt("How long would you like your password to be? (No more than 128 characters)");
+//   let numberResponse = Number(userResponse);
+//   while (isNaN(numberResponse) || numberResponse > 128 || numberResponse < 8) {
+//       if (isNaN(numberResponse)) {
+//         userResponse = prompt("That's not a number. Please enter a number.");
+//       }
+//       else if(numberResponse > 128) {
+//         userResponse = prompt("That's too many characters.");
+//       }
+//       else if (numberResponse < 8) {
+//         userResponse = prompt("That's not enough characters.");
+//       }
+
+//     }
+//     numberResponse = Number(userResponse);
+//   }
+
+// {
+// document.getElementById("generate").value=promptForLength;
+// }
 
 
 }
@@ -106,24 +141,11 @@ function writePassword() {
 
   passwordText.value = password;
 
-  //this asks the user to set the parameters of their password
-
-  confirm("Would you like your password to contain lower case letters?  Press 'ok' if so, 'cancel' if not.");
-
-  confirm("Would you like your password to contain upper case letters?  Press 'ok' if so, 'cancel' if not.");
-
-  confirm("Would you like your password to contain special characters?  Press 'ok' if so, 'cancel' if not.");
-
-  //this prompts users to type a length for the password.  OR operatory > 128 to add
 
 
-  let length = prompt("How long would you like your password to be?  Please choose a value of at least 8 characters and less than 128 characters.");
+  //this prompts users to type a length for the password.  OR operator > 128 to add
 
-  if (length < 8) {
-    alert("Please choose a value that is at least 8 characters and no more than 128 characters.");
-  } else { 
-     alert("nice one");
-  }
+
 
 
   /** 
